f8adc2023cfc9fbec15fdceb36422dca056558a9
Alexandr <shulyak.san@mail.ru>
2021-02-01 23:38:10 +1100

    did homework

--- home_work_2.py
+++ home_work_2.py
@@ -1,5 +1,6 @@
-time = input('Введите шестизначные цифры')
-def convert(min):
+time = int(input('Введите пятизначное число '))
+def convert(seconds):
+    min, sec = divmod(seconds, 60)
     hour, min = divmod(min, 60)
-    return
-print(convert)
+    return "%02d:%02d:%02d" %(hour, min, sec)
+print(convert(time))
